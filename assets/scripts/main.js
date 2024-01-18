8db4ade98f9af3f876a3f98863759e7c32a01f24
Tatien Miller <tatmil99@gmail.com>
2024-01-18 01:55:00 -0600

    changes name of index file to home. removes all redundant nav menus from all pages.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-const getCurrentPage = () => {
+const getActivePage = () => {
   if (window.location) {
     const path = window.location.pathname;
     const page = path.slice(1, -5);
@@ -7,39 +7,38 @@ const getCurrentPage = () => {
   }
 };
 
-const setNavColor = (currentPage) => {
+const highlightNav = (activePage) => {
   const navChildren = document.querySelector(".nav-list").children;
 
   for (const child of navChildren) {
-    if (child.classList.contains(currentPage)) {
+    if (child.classList.contains(activePage)) {
       child.classList.toggle("active-page");
     }
   }
 };
 
-const currentPage = getCurrentPage();
+const activePage = getActivePage();
+
+// create nav menu and append it to page
 window.addEventListener("load", () => {
   const nav = document.querySelector("nav");
   const ul = document.createElement("ul");
-  const pages = ["index", "gym", "tanning", "faqs", "contact"];
+  const pages = ["home", "gym", "tanning", "faqs", "contact"];
 
   const links = pages.map((page) => {
     const li = document.createElement("li");
     const a = document.createElement("a");
+    const capitalizedLink =
+      page === "faqs"
+        ? page.slice(0, -1).toUpperCase() + page[page.length - 1]
+        : page[0].toUpperCase() + page.slice(1);
 
-    if (page === "index") {
-      li.classList.add("nav-link", "first-link", page);
-      a.href = `../${page}.html`;
-      a.textContent = "Home";
-    } else {
-      const capitalizedLink = page[0].toUpperCase() + page.slice(1);
-      li.classList.add("nav-link", page);
-      a.href = `../${page}.html`;
-      a.textContent = `${capitalizedLink}`;
-    }
+    if (page === "home") li.classList.add("nav-link", "first-link", page);
+    li.classList.add("nav-link", page);
+    a.href = `../${page}.html`;
+    a.textContent = `${capitalizedLink}`;
 
     li.appendChild(a);
-
     return li;
   });
 
@@ -47,5 +46,5 @@ window.addEventListener("load", () => {
   ul.append(...links);
   nav.appendChild(ul);
 
-  setNavColor(currentPage);
+  highlightNav(activePage);
 });
