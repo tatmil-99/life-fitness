54ec81e0b82d4b63ff67dba3a3e08fa6ab57bc80
Tatien Miller <tatmil99@gmail.com>
2024-01-18 01:15:51 -0600

    abstracts logic for setting nav link attributes into a map function for easier understanding and better logic

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,51 +18,30 @@ const setNavColor = (currentPage) => {
 };
 
 const currentPage = getCurrentPage();
-
 window.addEventListener("load", () => {
   const nav = document.querySelector("nav");
   const ul = document.createElement("ul");
-  const links = [];
+  const pages = ["index", "gym", "tanning", "faqs", "contact"];
 
-  let i = 0;
-  while (i < 5) {
+  const links = pages.map((page) => {
     const li = document.createElement("li");
     const a = document.createElement("a");
 
-    // could break this into a function
-    switch (i) {
-      case 0:
-        li.classList.add("nav-link", "first-link", "index");
-        a.href = "../index.html";
-        a.textContent = "Home";
-        break;
-      case 1:
-        li.classList.add("nav-link", "gym");
-        a.href = "../gym.html";
-        a.textContent = "Gym";
-        break;
-      case 2:
-        li.classList.add("nav-link", "tanning");
-        a.href = "../tanning.html";
-        a.textContent = "Tanning";
-        break;
-      case 3:
-        li.classList.add("nav-link", "faqs");
-        a.href = "../faqs.html";
-        a.textContent = "FAQs";
-        break;
-      case 4:
-        li.classList.add("nav-link", "contact");
-        a.href = "../contact.html";
-        a.textContent = "Contact";
-        break;
+    if (page === "index") {
+      li.classList.add("nav-link", "first-link", page);
+      a.href = `../${page}.html`;
+      a.textContent = "Home";
+    } else {
+      const capitalizedLink = page[0].toUpperCase() + page.slice(1);
+      li.classList.add("nav-link", page);
+      a.href = `../${page}.html`;
+      a.textContent = `${capitalizedLink}`;
     }
 
     li.appendChild(a);
-    links.push(li);
 
-    i++;
-  }
+    return li;
+  });
 
   ul.classList.add("nav-list");
   ul.append(...links);
