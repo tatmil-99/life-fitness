111ba14fbe64ad2dae74a10ef8ce8e9c151823e6
Tatien Miller <tatmil99@gmail.com>
2024-01-30 17:44:54 -0600

    request to server displays page, but styles are missing

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const http = require("http");
 const fs = require("fs");
-const url = require("url");
 
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
+  const parsedUrl = new URL(req.url, "http://localhost:8080");
 
-  fs.readFile(parsedUrl.pathname, (err, data) => {
+  fs.readFile("." + parsedUrl.pathname, (err, data) => {
     if (err) {
-      res.statusCode = 400;
-      res.setHeader("Content-Type", "text/html");
-      return res.end(statusCode + " not found");
+      res.writeHead(404, { "Content-Type": "text/html" });
+      return res.end("404 not found");
     }
 
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.write(data);
     return res.end();
   });
