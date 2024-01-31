3de8210190730c931eddad6cab0a1dd472448c18
Tatien Miller <tatmil99@gmail.com>
2024-01-30 18:12:24 -0600

    fixes issues serving styles/scripts by setting header content type according to file type

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ const server = http.createServer((req, res) => {
     if (err) {
       res.writeHead(404, { "Content-Type": "text/html" });
       return res.end("404 not found");
+    } else if (parsedUrl.pathname.includes(".css")) {
+      res.writeHead(200, { "Content-Type": "text/css" });
+    } else if (parsedUrl.pathname.includes(".js")) {
+      res.writeHead(200, { "Content-Type": "application/javascript" });
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
     }
 
-    res.writeHead(200, { "Content-Type": "text/html" });
     res.write(data);
     return res.end();
   });
