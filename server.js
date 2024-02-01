7d5eabfd217c0c5134a518bed3647a267c8696a2
Tatien Miller <tatmil99@gmail.com>
2024-02-01 06:04:20 -0600

    creates variable for content-type of header to abstract logic

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-  const parsedUrl = new URL(req.url, "http://localhost:8080");
-  const urlPath = "." + parsedUrl.pathname;
+  const filePath = path.join(
+    __dirname,
+    "public",
+    req.url == "/" ? "home.html" : req.url
+  );
+  const fileExt = path.extname(filePath);
 
-  fs.readFile(urlPath, (err, data) => {
+  let contentType;
+
+  if (fileExt == ".js") {
+    contentType = "text/javascript";
+  } else if (fileExt == ".jpg" || fileExt == ".png") {
+    contentType = `image/${fileExt.slice(1)}`;
+  } else {
+    contentType = `text/${fileExt.slice(1)}`;
+  }
+
+  fs.readFile(filePath, (err, data) => {
     try {
-      res.statusCode = 200;
-
-      // set header content based on media type
-      if (urlPath.includes(".css")) {
-        res.setHeader("Content-Type", "text/css");
-      } else if (urlPath.includes(".js")) {
-        res.setHeader("Content-Type", "text/javascript");
-      } else {
-        res.setHeader("Content-Type", "text/html");
-      }
-
+      res.writeHead(200, { "Content-Type": contentType });
       res.write(data);
-
-      return res.end();
     } catch (err) {
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/html");
-
-      return res.end("404 not found");
+      if (err.code == "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write("<h1>404 Not Found</h1>");
+      }
     }
+
+    return res.end();
   });
 });
 
