9ce31887608792fa52ad5a8d43bb4a0c40a85283
Tatien Miller <tatmil99@gmail.com>
2024-02-06 04:42:31 -0600

    adds fetch api to get first image from server when carousel is swiped right.

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,23 +1,33 @@
 const handleStart = (e) => {
   e.preventDefault();
-  const startX = e.changedTouches[0].clientX;
 
   const img = e.target;
+  const startX = e.changedTouches[0].clientX;
 
   const handleEnd = (e) => {
     e.preventDefault();
+
     const endX = e.changedTouches[0].clientX;
 
     if (endX < startX) {
-      console.log("swiped right");
+      fetch("./images/_DSC8161.JPG")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+          }
+          return response.blob();
+        })
+        .then((myBlob) => {
+          const objectURL = URL.createObjectURL(myBlob);
+          img.src = objectURL;
+        })
+        .catch((err) => console.error(`Fetch problem: ${err.message}`));
     } else if (endX > startX) {
       console.log("swiped left");
     }
-
     // removes event listener to prevent build up of "touchend" events
     img.removeEventListener("touchend", handleEnd);
   };
-
   img.addEventListener("touchend", handleEnd);
 };
 
