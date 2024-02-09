34340db03e600b283c5602b0d9ff501db8d0b8db
Tatien Miller <tatmil99@gmail.com>
2024-02-09 04:57:37 -0600

    carousel kind of works but there is a lag when swiping the opposite direction of previous swipe

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,6 +1,6 @@
-const viewedImages = ["../images/_DSC8166.JPG"];
-
 const imageFiles = ["../images/_DSC8151.JPG", "../images/_DSC8161.JPG"];
+
+const viewedImages = ["../images/_DSC8166.JPG"];
 
 const preLoadImg = (file) => {
   const img = new Image();
@@ -13,8 +13,11 @@ const preLoadImg = (file) => {
 };
 
 const handleStart = (e) => {
-  let file = imageFiles.length - 1;
-  const preLoadedImg = preLoadImg(imageFiles[file]);
+  const nextFile = imageFiles.length - 1;
+  const preLoadedImg = preLoadImg(imageFiles[nextFile]);
+  const prevFile = viewedImages.length - 1;
+  const prevImg = preLoadImg(viewedImages[prevFile]);
+
   const img = e.target;
 
   const startX = e.changedTouches[0].clientX;
@@ -22,22 +25,28 @@ const handleStart = (e) => {
     const endX = e.changedTouches[0].clientX;
 
     // swipe right and replace img if files are available
-    if (endX < startX && file !== -1) {
+    if (endX < startX && imageFiles.length !== 0) {
+      console.log("SWIPED RIGHT");
+
       img.replaceWith(preLoadedImg);
       viewedImages.push(imageFiles.pop());
 
-      preLoadedImg.addEventListener("touchstart", handleStart);
-
-      console.log("viewed images: ", viewedImages);
       console.log("image files left: ", imageFiles);
-    } else if (endX > startX) {
-      console.log("swiped left");
-    }
+      console.log("viewed images: ", viewedImages);
+    } else if (endX > startX && viewedImages.length !== 0) {
+      console.log("SWIPED LEFT");
 
-    // removes event listener to prevent build up of "touchend" events
-    img.removeEventListener("touchend", handleEnd);
+      img.replaceWith(prevImg);
+      imageFiles.push(viewedImages.pop());
+
+      console.log("image files left: ", imageFiles);
+      console.log("viewed images: ", viewedImages);
+    }
   };
 
+  preLoadedImg.addEventListener("touchstart", handleStart);
+  prevImg.addEventListener("touchstart", handleStart);
+  img.removeEventListener("touchend", handleEnd); // prevent piling of events
   img.addEventListener("touchend", handleEnd);
 };
 
