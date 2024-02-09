1698ae1617a3860f26a08bd2cceb672952fb60e9
Tatien Miller <tatmil99@gmail.com>
2024-02-08 19:11:51 -0600

    adds array of viewed images and image files and moves files to viewed images as they are interacted with.

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,26 +1,35 @@
-const imageFiles = ["../images/_DSC8161.JPG"];
+const viewedImages = ["../images/_DSC8166.JPG"];
+
+const imageFiles = ["../images/_DSC8151.JPG", "../images/_DSC8161.JPG"];
 
 const preLoadImg = (file) => {
   const img = new Image();
   img.src = file;
   img.className = "carousel";
+  img.width = "750";
+  img.height = "500";
 
   return img;
 };
 
-const preLoadedImg = preLoadImg(imageFiles[0]);
-
 const handleStart = (e) => {
+  let file = imageFiles.length - 1;
+  const preLoadedImg = preLoadImg(imageFiles[file]);
   const img = e.target;
-  const startX = e.changedTouches[0].clientX;
 
+  const startX = e.changedTouches[0].clientX;
   const handleEnd = (e) => {
     const endX = e.changedTouches[0].clientX;
 
-    if (endX < startX) {
-      console.log("swiped right");
+    // swipe right and replace img if files are available
+    if (endX < startX && file !== -1) {
       img.replaceWith(preLoadedImg);
+      viewedImages.push(imageFiles.pop());
+
       preLoadedImg.addEventListener("touchstart", handleStart);
+
+      console.log("viewed images: ", viewedImages);
+      console.log("image files left: ", imageFiles);
     } else if (endX > startX) {
       console.log("swiped left");
     }
