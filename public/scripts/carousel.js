ec12159b0ed6d9a774f6afb00940ddfaf99acae6
Tatien Miller <tatmil99@gmail.com>
2024-02-13 03:32:35 -0600

    re-writes program to preload image **before** it is used in event handler

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,6 +1,40 @@
-const imageFiles = ["../images/_DSC8151.JPG", "../images/_DSC8161.JPG"];
+// const handleStart = (e) => {
+//   const nextFile = imageFiles.length - 1;
+//   const preLoadedImg = preLoadImg(imageFiles[nextFile]);
+//   const prevFile = viewedImages.length - 1;
+//   const prevImg = preLoadImg(viewedImages[prevFile]);
 
-const viewedImages = ["../images/_DSC8166.JPG"];
+//   const img = e.target;
+
+//   const startX = e.changedTouches[0].clientX;
+//   const handleEnd = (e) => {
+//     const endX = e.changedTouches[0].clientX;
+
+//     // swipe right and replace img if files are available
+//     if (endX < startX && imageFiles.length !== 0) {
+//       console.log("SWIPED RIGHT");
+
+//       img.replaceWith(preLoadedImg);
+//       viewedImages.push(imageFiles.pop());
+
+//       console.log("image files left: ", imageFiles);
+//       console.log("viewed images: ", viewedImages);
+//     } else if (endX > startX && viewedImages.length !== 0) {
+//       console.log("SWIPED LEFT");
+
+//       img.replaceWith(prevImg);
+//       imageFiles.push(viewedImages.pop());
+
+//       console.log("image files left: ", imageFiles);
+//       console.log("viewed images: ", viewedImages);
+//     }
+//   };
+
+//   preLoadedImg.addEventListener("touchstart", handleStart);
+//   prevImg.addEventListener("touchstart", handleStart);
+//   img.removeEventListener("touchend", handleEnd); // prevent piling of events
+//   img.addEventListener("touchend", handleEnd);
+// };
 
 const preLoadImg = (file) => {
   const img = new Image();
@@ -13,42 +47,39 @@ const preLoadImg = (file) => {
 };
 
 const handleStart = (e) => {
-  const nextFile = imageFiles.length - 1;
-  const preLoadedImg = preLoadImg(imageFiles[nextFile]);
-  const prevFile = viewedImages.length - 1;
-  const prevImg = preLoadImg(viewedImages[prevFile]);
-
   const img = e.target;
 
   const startX = e.changedTouches[0].clientX;
   const handleEnd = (e) => {
     const endX = e.changedTouches[0].clientX;
 
-    // swipe right and replace img if files are available
-    if (endX < startX && imageFiles.length !== 0) {
-      console.log("SWIPED RIGHT");
-
+    // swipe right and replace img
+    if (endX < startX && nextFile) {
       img.replaceWith(preLoadedImg);
-      viewedImages.push(imageFiles.pop());
-
-      console.log("image files left: ", imageFiles);
-      console.log("viewed images: ", viewedImages);
-    } else if (endX > startX && viewedImages.length !== 0) {
-      console.log("SWIPED LEFT");
-
+      nextFile = imageFiles.pop();
+      preLoadedImg = preLoadImg(nextFile);
+      preLoadedImg.addEventListener("touchstart", handleStart);
+    } else if (endX > startX) {
       img.replaceWith(prevImg);
-      imageFiles.push(viewedImages.pop());
-
-      console.log("image files left: ", imageFiles);
-      console.log("viewed images: ", viewedImages);
     }
   };
 
-  preLoadedImg.addEventListener("touchstart", handleStart);
-  prevImg.addEventListener("touchstart", handleStart);
   img.removeEventListener("touchend", handleEnd); // prevent piling of events
   img.addEventListener("touchend", handleEnd);
 };
 
+const imageFiles = [
+  "../images/_DSC8166.JPG",
+  "../images/_DSC8151.JPG",
+  "../images/_DSC8161.JPG",
+];
+
+const viewedImages = ["../images/_DSC8166.JPG"];
+
+let nextFile = imageFiles.pop();
+let preLoadedImg = preLoadImg(nextFile);
+
 const img = document.querySelector(".slideshow-img");
 img.addEventListener("touchstart", handleStart);
+
+preLoadedImg.addEventListener("touchstart", handleStart);
