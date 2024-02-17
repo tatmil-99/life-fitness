41084c945e19be86e15dd51593247e6243fa8208
Tatien Miller <tatmil99@gmail.com>
2024-02-17 05:36:22 -0600

    pre-renders all files and adds event listeners to each rendered object outside of event handler itself. uses fifo for displaying viewed and non-viewed images as the user interacts with the carousel.

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,37 +1,17 @@
-const preLoadImg = (file) => {
-  const img = new Image();
-  img.src = file;
-  img.className = "carousel";
-  img.width = "750";
-  img.height = "500";
-
-  return img;
-};
-
 const handleStart = (e) => {
   const img = e.target;
-
-  // use x-coordinates to determine swipe direction
   const startX = e.changedTouches[0].clientX;
+  
+  // use x-coordinates to determine swipe direction
   const handleEnd = (e) => {
     const endX = e.changedTouches[0].clientX;
 
-    // display image if it has already been rendered
-    if (endX < startX && imgIndex < viewedImages.length) {
-      img.replaceWith(viewedImages[imgIndex]);
-      imgIndex++;
-    } else if (endX < startX && nextFile) {      
-      img.replaceWith(preLoadedImg);
-      viewedImages.push(preLoadedImg);
-      
-      nextFile = imageFiles.pop();
-      preLoadedImg = preLoadImg(nextFile);
-      preLoadedImg.addEventListener("touchstart", handleStart);
-      
-      imgIndex++;
-    } else if (endX > startX && imgIndex > 0) {
-      imgIndex--;
-      img.replaceWith(viewedImages[imgIndex - 1]);
+    if (endX < startX) {
+      img.replaceWith(preLoadedImages.shift());
+      viewedImages.unshift(img)
+    } else if (endX > startX) {
+      img.replaceWith(viewedImages.shift());
+      preLoadedImages.unshift(img);
     }
 
     img.removeEventListener("touchend", handleEnd); // prevent piling of events
@@ -40,21 +20,27 @@ const handleStart = (e) => {
   img.addEventListener("touchend", handleEnd);
 };
 
-const imageFiles = [
-  "../images/_DSC8151.JPG",
-  "../images/_DSC8161.JPG",
-];
-// how do i handle initially loaded img?
+
 const viewedImages = [];
-// index starts counting after first rendered image
-// index will be "one-off"
-let imgIndex = 0;
-
-let nextFile = imageFiles.pop();
-let preLoadedImg = preLoadImg(nextFile);
-
 
 const img = document.querySelector(".slideshow-img");
 img.addEventListener("touchstart", handleStart);
 
-preLoadedImg.addEventListener("touchstart", handleStart);
+const imageFiles = [
+  "../images/_DSC8151.JPG",
+  "../images/_DSC8161.JPG",
+];
+
+const preLoadedImages = imageFiles.map(file => {
+  const img = new Image();
+  img.src = file;
+  img.className = "carousel";
+  img.width = "750";
+  img.height = "500";
+
+  return img;
+});
+
+preLoadedImages.forEach(img => {
+  img.addEventListener("touchstart", handleStart)
+});
